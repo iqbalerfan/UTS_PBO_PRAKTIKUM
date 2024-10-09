306cd6e6c770f8e84ee52ef004ae0e1ed588da1c
Iqbal Erfansyah <145417561+iqbalerfan@users.noreply.github.com>
2024-10-09 17:33:57 +0700

    Add files via upload

---
+++ UTS_FINAL/ReservasiManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package UTS_FINAL;
+
+/**
+ *
+ * @author iqbal
+ */
+public interface ReservasiManager {
+    void buatReservasi(Kamar kamar, Customer customer);
+    void lihatReservasi(Customer customer);
+}
